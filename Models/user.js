60a00bbb022830007c75a29fb056c7257a7b4c33
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-20 09:42:41 -0800

    created schema, started on models, views and controllers

---
+++ Models/user.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+class User extends Model {};
+
+User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      },
+    },
+)
