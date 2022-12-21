b47ce8bc54f117bc7bbb0bed84686bbc674edae0
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-20 20:16:27 -0800

    created API routes, working on front end routes

--- Controllers/api/entryroutes.js
+++ Controllers/api/entryroutes.js
@@ -1,4 +1,57 @@
 const router = require('express').Router();
 const { User, Entry, Comment } = require('../../Models')
 
-router.get('/:id')
+router.get('/:id', (req, res) => {
+    Entry.findByPk(req.params.id, {
+        include: Comment
+    })
+}).then((entry) => {
+    res.json(entry)
+}).catch((err) => {
+    console.log(err);
+    res.status(500).json({ err: err })
+});
+
+router.post('/', async (req, res) => {
+
+    if (!req.session.logged_in) {
+        return res.status(401).json({ msg: "You've gotta log in for that!" })
+    }
+    try {
+        const newEntry = await Entry.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newEntry);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+
+
+    if (!req.session.logged_in) {
+        return res.status(401).json({ msg: "You've gotta log in for that!" })
+    }
+    try {
+        const entryData = await Entry.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!entryData) {
+            res.status(404).json({ message: 'No entry found with this id' })
+            return;
+        }
+
+        res.status(200).json(entryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- Controllers/api/userroutes.js
+++ Controllers/api/userroutes.js
@@ -1,32 +1,66 @@
 const router = require('express').Router();
-const { User, Entry } = require('../../models');
-const bcrypt = require('bcrypt');
+const { User, Entry, Comment } = require('../../models');
 
-
-//gets all users
-router.get('/', (req, res => {
-    res.json(userData)
-}).catch(err => res.status(500).json({ msg: "something went wrong", err })
-))
-
-//creates new user
-router.post('/', (req, res => {
-    console.log(req.body);
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(newUser => {
-        req.session.userId = newUser.id;
-        req.session.loggedIn = true;
-        res.json(newUser)
+router.get('/', (req, res) => {
+    User.findAll({
+        include: [Entry, Comment]
+    }).then(userData => {
+        res.json(userData)
     }).catch(err => {
-        console.log(err);
-        res.status(500).json({ err })
+        res.status(500).json({ msg: "An error has occurred", err })
     })
-}))
+});
 
-//gets user by id
-router.get('/id', (req, res) => {
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = userData.id
+            req.session.logged_in = true;
 
-})
+            res.status(200).json(userData)
+        })
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password' });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are successfully logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+
+module.exports = router;
